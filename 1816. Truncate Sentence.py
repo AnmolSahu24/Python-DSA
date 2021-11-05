16b270b5f1fb258bb3571fb267df4c5bbc11696d
Anmol <anmolsahu99@gmail.com>
2021-11-05 15:17:18 +0530

    Array from LeetCode

---
+++ 1816. Truncate Sentence.py
@@ -0,0 +1,8 @@
+
+def TruncateSentance(str,k):
+    string_ = list(str.split(" "))
+    print(" ".join(string_[:k]))
+    return " ".join(string_[:k])
+
+
+TruncateSentance("Hello how are you Contestant",4)
