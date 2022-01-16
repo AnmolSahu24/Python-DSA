023c6ce1626051f976d9f5a4a176c585669f7fb3
Anmol <anmolsahu99@gmail.com>
2022-01-16 14:39:17 +0530

    Array Leetcode

--- Array/1389. Create Target Array in the Given Order.py
+++ Array/1389. Create Target Array in the Given Order.py
@@ -1,1 +1,14 @@
-def
+def creatTarget (nums, index):
+    n = len(nums)
+    res = []
+    i = 0
+    while i < n:
+            res.insert(index[i], nums[i])
+
+            i+=1
+    return res
+
+
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+print(creatTarget(nums,index))
